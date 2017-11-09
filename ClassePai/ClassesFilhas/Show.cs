0265317fd129a18990703771f64192fd35ec3ae5
FIC <FIC@info.local>
2017-11-09 20:33:11 -0200

    Aula 18

---
+++ ClassePai/ClassesFilhas/Show.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ProjetoEvento.ClassePai.ClassesFilhas
+{
+    public class Show : Evento
+    {
+        public string Artista { get; set; }
+        public string GeneroMusical { get; set; }
+        //constrtutor simples e/ou com passagem de dados
+        public Show() { } // construtor simples só instancia a sua classe
+        public Show(string Titulo, string Local, int Lotacao, string Duracao, int Classificacao, DateTime Data, string Artista, string GeneroMusical)
+        {
+            base.Titulo = Titulo;
+            base.Local = Local;
+            base.Lotacao = Lotacao;
+            base.Duracao = Duracao;
+            base.Classificacao = Classificacao;
+
+            this.Artista = Artista;
+            this.GeneroMusical = GeneroMusical;
+        }
+
+        public override bool Cadastrar()
+        {
+            bool efetuado = false;
+            StreamWriter arquivo = null;
+            try
+            {
+                arquivo = new StreamWriter("show.csv", true);
+                arquivo.WriteLine(
+                    Titulo + ";" +
+                    Local + ";" +
+                    Duracao + ";" +
+                    Data + ";" +
+                    Artista + ";" +
+                    GeneroMusical + ";" +
+                    Lotacao + ";" +
+                    Classificacao
+                    );
+                efetuado = true;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Erro ao tentar gravar o arquivo. "+ex.Message);
+            }
+            finally
+            {
+                arquivo.Close();
+            }
+            return efetuado;
+        }
+    
+        public override string Pesquisar(string Titulo)
+        {
+            string resultado = "Titulo não encontrado!";
+
+            StreamReader ler = null;
+
+            try{
+            ler = new StreamReader("show.csv",Encoding.Default);
+
+            string linha ="";
+
+            while((linha=ler.ReadLine())!=null)
+            {
+                string[] dados = linha.Split(';');
+                if(dados[0]==Titulo)
+                {
+                    resultado = linha;
+                    break;
+                }
+            }
+            }
+            catch(Exception ex){
+                resultado = "Erro ao tentar ler o arquivo! "+ex.Message;
+            }
+            finally{
+                ler.Close();
+            }
+            return resultado;
+        }
+    public override string Pesquisar(DateTime Data)
+        {
+            string resultado = "Data não encontrado!";
+
+            StreamReader ler = null;
+
+            try{
+            ler = new StreamReader("show.csv",Encoding.Default);
+
+            string linha ="";
+
+            while((linha=ler.ReadLine())!=null)
+            {
+                string[] dados = linha.Split(';');
+                if(dados[3]==Data.ToString())
+                {
+                    resultado = linha;
+                    break;
+                }
+            }
+            }
+            catch(Exception ex){
+                resultado = "Erro ao tentar ler o arquivo! "+ex.Message;
+            }
+            finally{
+                ler.Close();
+            }
+            return resultado;
+        }
+
+    }
+}
---
+++ ClassePai/Evento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ProjetoEvento.ClassePai
+{
+    public abstract class Evento
+    {
+        public string Titulo { get; set; }
+        public int Lotacao { get; set; }
+        public string Local { get; set; }
+        public string Duracao { get; set; }
+        public DateTime Data { get; set; }
+        public int Classificacao { get; set; }
+
+        
+        public virtual bool Cadastrar(){
+            return false;
+        }
+
+        public virtual string Pesquisar(string Titulo){
+            return null;
+        }
+        public virtual string Pesquisar(DateTime Data){
+            return null;
+        }
+    }
+}
